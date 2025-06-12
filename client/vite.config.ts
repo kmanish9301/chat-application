7e31b6fb6811a1069e9062a5a247b97d0b15255c
kmanish9301 <manishkharbade@gmail.com>
2025-06-12 18:23:43 +0530

    client build failed fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    port: 5174,
     proxy: {
       "/v1": {
         target: "http://localhost:5000", // local dev
